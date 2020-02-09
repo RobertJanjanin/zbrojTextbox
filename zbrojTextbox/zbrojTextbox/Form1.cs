343460a78c2fbe5ef0d7cbf6d4c6a6d2711b4c8c
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-09 19:29:14 +0100

    Add files via upload

---
+++ zbrojTextbox/zbrojTextbox/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zbrojTextbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int prvi = 0;
+            int drugi = 0;
+
+            if (int.TryParse(textBox1.Text, out prvi))
+            {
+
+                if (int.TryParse(textBox2.Text, out drugi))
+                {
+                    MessageBox.Show("Rezultat je  " + (prvi + drugi));
+                }
+                else
+                {
+                    MessageBox.Show("Morate upisati broj");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Morate upisati broj");
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
